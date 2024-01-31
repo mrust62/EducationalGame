21996db2b63e1c082cc784eeb9508b60578c6bb1
mrust62 <111906550+mrust62@users.noreply.github.com>
2024-01-31 12:13:09 -0500

    Added files

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject[] Levels;
+
+    int currentLevel;
+
+    public void correctAnswer()
+    {
+        if (currentLevel + 1 != Levels.Length)
+        {
+            Levels[currentLevel].SetActive(false);
+
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    string clickedButton = "";
+    string clickedButton2 = "";
+
+    public void correctMatch()
+    {
+
+        if (clickedButton == "")
+        {
+            clickedButton = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+            Debug.Log(clickedButton);
+        }
+        else if (clickedButton2 == "")
+        {
+            clickedButton2 = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+            Debug.Log(clickedButton2);
+
+            if (clickedButton == "Gold" && clickedButton2 == "Au")
+            {
+                Debug.Log("CORRECT");
+                clickedButton = "";
+                clickedButton2 = "";
+            }
+            else if (clickedButton == "Hydrogen" && clickedButton2 == "H")
+            {
+                Debug.Log("CORRECT");
+                clickedButton = "";
+                clickedButton2 = "";
+            }
+            else if (clickedButton == "Titanium" && clickedButton2 == "Ti")
+            {
+                Debug.Log("CORRECT");
+                clickedButton = "";
+                clickedButton2 = "";
+            }
+            else if (clickedButton == "Carbon" && clickedButton2 == "C")
+            {
+                Debug.Log("CORRECT");
+                clickedButton = "";
+                clickedButton2 = "";
+            }
+            else
+            {
+                Debug.Log("INCORRECT");
+                clickedButton = "";
+                clickedButton2 = "";
+            }
+
+
+        }
+
+
+
+
+    }
+}
